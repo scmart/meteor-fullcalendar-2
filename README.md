e1d179f36d4b590ffa0b88a475f3f161eec42acd
Sedric <sedric@torsh.co>
2014-04-30 13:53:36 -0700

    first release

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ meteor-fullcalendar-2
 =====================
 
 Fullcalendar for meteor (version 2)
+
+to install
+
+mrt add meteor-fullcalendar-2
---
+++ packages.js
@@ -0,0 +1,11 @@
+Package.describe({
+	summary: "fullcallendar v2 for Meteor - FullCalendar is a jQuery plugin that provides a full-sized, drag & drop calendar"
+});
+
+Package.on_use(function(api, where) {
+	api.use(['moment'], 'client');
+	api.add_files(['fullcalendar/fullcalendar.js', 
+				   'fullcalendar/fullcalendar.css',
+				   'fullcalendar/lang/all.js'
+				  ]);
+});
