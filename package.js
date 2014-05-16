1da43972a47a7d1cb1825b9be57b4feed4eb0ee5
Sedric <sedric@torsh.co>
2014-05-15 17:31:02 -0700

    switched to striped down jqueryui to avoid bootstrap conflicts

--- package.js
+++ package.js
@@ -3,8 +3,9 @@ Package.describe({
 });
 
 Package.on_use(function(api, where) {
-	api.use(['moment', 'jqueryui'], 'client');
-	api.add_files(['fullcalendar/fullcalendar.js', 
+	api.use(['moment'], 'client');
+	api.add_files(['lib/jquery-ui-1.10.4.custom.js',
+				   'fullcalendar/fullcalendar.js', 
 				   'fullcalendar/fullcalendar.css',
 				   'fullcalendar/lang/all.js'
 				  ], 'client');
